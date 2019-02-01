51798a83da781eea92eeb62aeece9e0151342efd
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-01 13:54:56 -0800

    Add schema to database

--- database/index.js
+++ database/index.js
@@ -1,16 +1,33 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  hmtl_url: String,
+  stargazers_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
+// makes a copy of schema
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+
+let save = (repoArr) => {
+  // we will call save on Repo, passing in the array that we get as a response from the github api GET request
+  // we want to map this array into a new array that contains objects with only the fields we want
+  var repoInfo = repoArr.map((repo) => {
+    return (
+      {
+        id,
+        name,
+        hmtl_url,
+        stargazers_count
+      }
+    );
+  });
+  // then we want to save each object from this array into Repo
+  Repo.insertMany(repoInfo);
 }
 
 module.exports.save = save;
