c11e8b08c04be459b25e6017d9fae01dc304a501
Elizabeth Cannon <elizabeth230@gmail.com>
2019-02-01 14:52:27 -0800

    Add some code for github helper method

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,23 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+// GET /users/:username/repos - lists all repos for user
+// URL should be base URL plus /users/{username}/repos
+
+let getReposByUsername = (username) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
-
+  
 }
 
 module.exports.getReposByUsername = getReposByUsername;
